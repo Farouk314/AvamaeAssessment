74ef2555faed27b642dca684a114ebe37fb7e24f
Farouk314 <fchousein@gmail.com>
2019-04-23 23:17:59 +0100

    WIP: Added polyfills

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import 'babel-polyfill'; 
+import 'react-app-polyfill/ie11';
 
 ReactDOM.render(<App />, document.getElementById('root'));
